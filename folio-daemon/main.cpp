Oneplus-Nord5-development/android_hardware_oplus

commit e9f090f2a6de61b40323b7979e3690e43841b5af

folio-daemon: PollOnce instead of PollAll
* Fixes build error.

Change-Id: I62019c3116a3ee697923595904f1d66d6842957b

folio-daemon/main.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright 2017 The Android Open Source Project
+ * Copyright 2023 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -132,7 +133,7 @@ int main(void) {
     ALOGI("Starting polling loop");
 
     // Polling loop
-    while (ALooper_pollAll(-1, NULL, NULL, NULL) == 0) {
+    while (ALooper_pollOnce(-1, NULL, NULL, NULL) == 0) {
         int eventCount = 0;
         ASensorEvent sensorEvent;
         while (ASensorEventQueue_getEvents(eventQueue, &sensorEvent, 1) > 0) {

commit 51adf2c0f4cb4098896a008a85eda22a2d51dcdd

folio-daemon: Fix ALooper_pollOnce() return value check
Fixes: e9f090f ("folio-daemon: PollOnce instead of PollAll")
Change-Id: I651a1614c85d0729f960a7dc524b45118d6de932

folio-daemon/main.cpp
@@ -117,7 +117,7 @@ int main(void) {
     ALOGI("Starting polling loop");
 
     // Polling loop
-    while (ALooper_pollOnce(-1, NULL, NULL, NULL) == 0) {
+    while (ALooper_pollOnce(-1, NULL, NULL, NULL) > ALOOPER_POLL_TIMEOUT) {
         int eventCount = 0;
         ASensorEvent sensorEvent;
         while (ASensorEventQueue_getEvents(eventQueue, &sensorEvent, 1) > 0) {