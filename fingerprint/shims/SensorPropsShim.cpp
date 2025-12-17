1fb2cd1ae259bef9e0a412bf3969c8115dd16d77
grepfox <grepfox@tutamail.com>
2025-12-17 13:41:28 +0000

    [dnm] Revert "fingerprint: Do not notify AIDL HAL about touch events"
    This reverts commit fd03aaaed975c034bc5645815fb6f9077ac2b0a2.

--- fingerprint/shims/SensorPropsShim.cpp
+++ fingerprint/shims/SensorPropsShim.cpp
@@ -61,10 +61,6 @@ SensorProps SensorPropsInit(SensorProps props) {
         }
     }
 
-    props.halHandlesDisplayTouches =
-            props.sensorType == FingerprintSensorType::UNDER_DISPLAY_OPTICAL ||
-            props.sensorType == FingerprintSensorType::UNDER_DISPLAY_ULTRASONIC;
-
     return props;
 }
 }  // anonymous namespace
